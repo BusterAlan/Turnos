b931a4869e671a39a96da3c116ad119ec3c3526c
Alan Chablé <1120030@alumno.um.edu.mx>
2024-11-13 16:39:04 -0600

    Commit de avance 2 (e42)

--- obj/Debug/net8.0/Turnos.AssemblyInfo.cs
+++ obj/Debug/net8.0/Turnos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Turnos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6470df4ffc40fb10427f943e46173424c9ce8885")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcaa506a5a88b0fe73dc409cf55c62ee721b9c36")]
 [assembly: System.Reflection.AssemblyProductAttribute("Turnos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Turnos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
