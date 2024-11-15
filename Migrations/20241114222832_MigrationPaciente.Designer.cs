8d75621014838dbccc70331586f77c1347097725
Alan Chablé <1120030@alumno.um.edu.mx>
2024-11-14 18:59:53 -0600

    Commit de avance 3 (e42 actividad 3)

--- Migrations/20241114222832_MigrationPaciente.Designer.cs
+++ Migrations/20241114222832_MigrationPaciente.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Turnos.Models;
 
@@ -10,9 +11,11 @@ using Turnos.Models;
 namespace Turnos.Migrations
 {
     [DbContext(typeof(TurnosContext))]
-    partial class TurnosContextModelSnapshot : ModelSnapshot
+    [Migration("20241114222832_MigrationPaciente")]
+    partial class MigrationPaciente
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Models/TurnosContext.cs
+++ Models/TurnosContext.cs
@@ -6,11 +6,11 @@ namespace Turnos.Models {
 
         public TurnosContext(DbContextOptions<TurnosContext> opciones) : base(opciones) {
 
-            
-
         }
 
         public DbSet<Especialidad>? Especialidad { get; set; }
+
+        public DbSet<Paciente>? Paciente { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -24,8 +24,42 @@ namespace Turnos.Models {
                 .IsRequired()
                 .HasMaxLength(200)
                 .IsUnicode(false);
-            }
-            );
+                
+            });
+
+            modelBuilder.Entity<Paciente>(entidad => 
+            {
+
+                entidad.ToTable("Paciente");
+
+                entidad.HasKey(p => p.IdPaciente);
+
+                entidad. Property (p => p.Nombre)
+                .IsRequired()
+                .HasMaxLength(50)
+                .IsUnicode(false);
+
+                entidad.Property(p => p.Apellido)
+                .IsRequired()
+                .HasMaxLength(50)
+                .IsUnicode(false);
+
+                entidad.Property(p => p.Direccion)
+                .IsRequired()
+                .HasMaxLength(250)
+                .IsUnicode(false);
+
+                entidad. Property (p => p.Telefono)
+                .IsRequired()
+                .HasMaxLength (20)
+                .IsUnicode(false);
+
+                entidad. Property (p => p.Email)
+                .IsRequired()
+                .HasMaxLength(100)
+                .IsUnicode(false);
+
+            });
 
         }
 
